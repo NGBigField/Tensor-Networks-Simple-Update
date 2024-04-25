1235e6af5c0cfc95d29214391824a4cfa3a34168
Nir Gutman <NGBigfield@gmail.com>
2024-04-25 10:33:50 -0400

    working relative imports of tnsu

--- scripts/AFH.py
+++ scripts/AFH.py
@@ -41,99 +41,124 @@ if not os.path.exists(save_path):
     os.makedirs(save_path)
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+"""
+### Import tnsu from this project
+"""
+
+
+from pathlib import Path
+
+scripts : Path = Path(__file__).parent
+main : Path = scripts.parent
+src : Path = main/"src"
+tnsu : Path = src/"tnsu"
+
+sys.path.append(src.__str__())
+
+from tnsu.tensor_network import TensorNetwork
+from tnsu import simple_update as su
+from tnsu import structure_matrix_constructor as smc
+
+
+
 
 """
 ### Finding the ground state with Tensor Network Simple Update
 """
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-from tnsu.tensor_network import TensorNetwork
-import tnsu.simple_update as su
-import tnsu.structure_matrix_constructor as smc
+def main():
+    np.random.seed(216)
+
+    # Pauli matrices
+    pauli_x = np.array([[0, 1],
+                        [1, 0]])
+    pauli_y = np.array([[0, -1j],
+                        [1j, 0]])
+    pauli_z = np.array([[1, 0],
+                        [0, -1]])
+    s_i = [pauli_x / 2., pauli_y / 2., pauli_z / 2.]
+    s_j = [pauli_x / 2., pauli_y / 2., pauli_z / 2.]
+    s_k = [pauli_x / 2.]
+
+    # The Tensor Network structure matrix
+    n = 2
+    structure_matrix = smc.square_peps_pbc(n)
+    print(f'There are {structure_matrix.shape[1]} edges, and {structure_matrix.shape[0]} tensors')
+
+    # AFH Hamiltonian interaction parameters
+    j_ij = [1.] * structure_matrix.shape[1]
+
+    # maximal bond dimension
+    d_max_ = [2]
+
+    # convergence error between consecutive lambda weights vectors
+    error = 1e-5
+
+    # maximal number of SU iterations
+    max_iterations = 200
+
+    # time intervals for the ITE
+    dts = [0.1, 0.01, 0.001, 0.0001, 0.00001]
+
+    # magnetic field weight (if 0, there is no magnetic field)
+    h_k = 0.
+
+    energies = []
+
+    # Run Simple Update
+    for d_max in d_max_:
+        # create Tensor Network name for saving
+        network_name = 'AFH_' + str(n) + 'x' + str(n) + '_pbc_' + 'D_' + str(d_max)
+
+        # create the Tensor Network object
+        AFH_TN = TensorNetwork(structure_matrix=structure_matrix,
+                            virtual_dim=2,
+                            network_name=network_name,
+                            dir_path=save_path)
+
+        # create the Simple Update environment
+        AFH_TN_su = su.SimpleUpdate(tensor_network=AFH_TN,
+                                    dts=dts,
+                                    j_ij=j_ij,
+                                    h_k=h_k,
+                                    s_i=s_i,
+                                    s_j=s_j,
+                                    s_k=s_k,
+                                    d_max=d_max,
+                                    max_iterations=max_iterations,
+                                    convergence_error=error,
+                                    log_energy=True,
+                                    print_process=True)
+
+        # run Simple Update algorithm over the Tensor Network state
+        AFH_TN_su.run()
+
+        # compute the energy per-site observable
+        energy = AFH_TN_su.energy_per_site()
+        print(f'| D max: {d_max} | Energy: {energy}\n')
+        energies.append(energy)
+
+        # save the tensor network
+        AFH_TN.save_network()
 
 
-np.random.seed(216)
+    """ 
+    ### Plot the Results
+    """
 
-# Pauli matrices
-pauli_x = np.array([[0, 1],
-                    [1, 0]])
-pauli_y = np.array([[0, -1j],
-                    [1j, 0]])
-pauli_z = np.array([[1, 0],
-                    [0, -1]])
-s_i = [pauli_x / 2., pauli_y / 2., pauli_z / 2.]
-s_j = [pauli_x / 2., pauli_y / 2., pauli_z / 2.]
-s_k = [pauli_x / 2.]
+    from tnsu.utils import plot_convergence_curve
 
-# The Tensor Network structure matrix
-n = 2
-structure_matrix = smc.square_peps_pbc(n)
-print(f'There are {structure_matrix.shape[1]} edges, and {structure_matrix.shape[0]} tensors')
-
-# AFH Hamiltonian interaction parameters
-j_ij = [1.] * structure_matrix.shape[1]
-
-# maximal bond dimension
-d_max_ = [4]
-
-# convergence error between consecutive lambda weights vectors
-error = 1e-5
-
-# maximal number of SU iterations
-max_iterations = 200
-
-# time intervals for the ITE
-dts = [0.1, 0.01, 0.001, 0.0001, 0.00001]
-
-# magnetic field weight (if 0, there is no magnetic field)
-h_k = 0.
-
-energies = []
-
-# Run Simple Update
-for d_max in d_max_:
-    # create Tensor Network name for saving
-    network_name = 'AFH_' + str(n) + 'x' + str(n) + '_pbc_' + 'D_' + str(d_max)
-
-    # create the Tensor Network object
-    AFH_TN = TensorNetwork(structure_matrix=structure_matrix,
-                           virtual_dim=2,
-                           network_name=network_name,
-                           dir_path=save_path)
-
-    # create the Simple Update environment
-    AFH_TN_su = su.SimpleUpdate(tensor_network=AFH_TN,
-                                dts=dts,
-                                j_ij=j_ij,
-                                h_k=h_k,
-                                s_i=s_i,
-                                s_j=s_j,
-                                s_k=s_k,
-                                d_max=d_max,
-                                max_iterations=max_iterations,
-                                convergence_error=error,
-                                log_energy=True,
-                                print_process=True)
-
-    # run Simple Update algorithm over the Tensor Network state
-    AFH_TN_su.run()
-
-    # compute the energy per-site observable
-    energy = AFH_TN_su.energy_per_site()
-    print(f'| D max: {d_max} | Energy: {energy}\n')
-    energies.append(energy)
-
-    # save the tensor network
-    AFH_TN.save_network()
+    # plot su convergence / energy curve
+    plot_convergence_curve(AFH_TN_su)
+    print("Done.")
 
 
-""" 
-### Plot the Results
-"""
-
-from tnsu.utils import plot_convergence_curve
-
-# plot su convergence / energy curve
-plot_convergence_curve(AFH_TN_su)
+if __name__=="__main__":
+    main()
